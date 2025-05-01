55b65a1e5dff9b7ffcadf69ddd3420f0be7a3c3b
Luiz Felipe Alves Costa <luiz.costa.1598724@sga.pucminas.br>
2025-05-01 12:33:22 -0300

    Atualizacao no Js

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ function carregar(){
     var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora =data.getHours()
-    msg.innerHTML = `Agora sao ${hora} horas`
+    msg.innerHTML = `Agora são ${hora} horas`
 
     if (hora >= 0 && hora < 12){
         //Bom dia!
